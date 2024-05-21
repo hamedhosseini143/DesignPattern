71d6056c8302fd18c1faecb653667e727cec87ef
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-05-21 15:56:18 +0330

    set index seperator

--- index.php
+++ index.php
@@ -5,9 +5,14 @@ require 'vendor/autoload.php';
 use src\DesignPattern\Creational\FactoryMethod\PaymentMethod\Client;
 use src\DesignPattern\Creational\FactoryMethod\PaymentMethod\Order;
 
+
+/** start factory method pattern  */
+
 $order = new Order(100, true);
 $paypal = new Client('paypal', $order);
 $paypal->processPayment();
 echo '<br>';
 $creditCard = new Client('credit_card', $order);
 $creditCard->processPayment();
+
+/** end factory method pattern  */
