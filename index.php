1956e0c348b94834536e0fcd6a8e583e361bda86
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-05-21 15:48:40 +0330

    test credit_card method

--- index.php
+++ index.php
@@ -6,6 +6,8 @@ use src\DesignPattern\Creational\FactoryMethod\PaymentMethod\Client;
 use src\DesignPattern\Creational\FactoryMethod\PaymentMethod\Order;
 
 $order = new Order(100, true);
-$client = new Client('paypal', $order);
-
-$client->processPayment();
+$paypal = new Client('paypal', $order);
+$paypal->processPayment();
+echo '<br>';
+$creditCard = new Client('credit_card', $order);
+$creditCard->processPayment();
