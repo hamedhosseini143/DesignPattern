4ed349ffeb19fd088d9f7b41f00e2900d9d56578
drupaldev143 <119724661+drupaldev143@users.noreply.github.com>
2024-05-28 09:18:49 +0330

    edite index.php foe clean code

--- index.php
+++ index.php
@@ -2,89 +2,81 @@
 
 require 'vendor/autoload.php';
 
+use src\DesignPattern\BehavioralPatterns\Command\Tasks\ClientCommandPattern;
 use src\DesignPattern\BehavioralPatterns\TemplateMethod\Document\ClientDocument;
 use src\DesignPattern\CreationalPatterns\AbstractFactory\FormBuilder\FormBuilderClient;
 use src\DesignPattern\CreationalPatterns\Builder\RequestBuilder\ApiService;
 use src\DesignPattern\CreationalPatterns\FactoryMethod\PaymentMethod\Client;
 use src\DesignPattern\CreationalPatterns\FactoryMethod\PaymentMethod\Order;
 
-
-/** start factory method pattern  */
-echo "/** start factory method pattern  */";
-echo "<br>";
-$order = new Order(100, true);
-$paypal = new Client('paypal', $order);
-$paypal->processPayment();
-echo '<br>';
-$creditCard = new Client('credit_card', $order);
-$creditCard->processPayment();
-echo "<br>";
-echo "/** end factory method pattern  */";
-echo "<br>";
-
-
-/** end factory method pattern  */
-
-/** start abstract factory pattern  */
-echo "/** start abstract factory pattern  */";
-echo "<br>";
-$form = new FormBuilderClient();
-$form->buildBootstrapForm();
-echo '<br>';
-$form->buildTailwindForm();
-echo "<br>";
-echo "/** end abstract factory pattern  */";
-
-/** end abstract factory pattern  */
-
-/** start builder pattern  */
-
-echo "<br>";
-echo "/** start builder pattern  */";
-echo "<br>";
-$request = new ApiService();
-$user = $request->fetchUser();
-echo "<pre>";
-print_r($user);
-echo "</pre>";
-echo "<br>";
-echo "/** end builder pattern  */";
-echo "<br>";
-/** end builder pattern  */
-
-
-/** start template method pattern  */
-echo "/** start template method pattern  */";
-echo "<br>";
-
-$document = new ClientDocument('word');
-try {
-    $document->generateDocument();
-} catch (Exception $e) {
-    echo $e->getMessage();
+/**
+ * @return void
+ */
+function demoFactoryMethodPattern(): void
+{
+    echo "<br>/** Factory Method Pattern */<br>";
+    $order = new Order(100, true);
+    (new Client('paypal', $order))->processPayment();
+    echo '<br>';
+    (new Client('credit_card', $order))->processPayment();
 }
 
-echo "<br>";
-$document = new ClientDocument('pdf');
-
-try {
-    $document->generateDocument();
-} catch (Exception $e) {
-    echo $e->getMessage();
+/**
+ * @return void
+ */
+function demoAbstractFactoryPattern(): void
+{
+    echo "<br>/** Abstract Factory Pattern */<br>";
+    $form = new FormBuilderClient();
+    $form->buildBootstrapForm();
+    echo '<br>';
+    $form->buildTailwindForm();
 }
 
-echo "<br>";
-echo "/** end template method pattern  */";
+/**
+ * @return void
+ */
+function demoBuilderPattern(): void
+{
+    echo "<br>/** Builder Pattern */<br>";
+    $request = new ApiService();
+    $user = $request->fetchUser();
+    echo "<pre>" . print_r($user, true) . "</pre>";
+}
 
-/** end template method pattern  */
-/** start command pattern  */
-echo "/** start command pattern  */";
-echo "<br>";
+/**
+ * @return void
+ */
+function demoTemplateMethodPattern(): void
+{
+    echo "<br>/** Template Method Pattern */<br>";
+    try {
+        (new ClientDocument('word'))->generateDocument();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+    echo "<br>";
+    try {
+        (new ClientDocument('pdf'))->generateDocument();
+    } catch (Exception $e) {
+        echo $e->getMessage();
+    }
+}
 
-$addTaskCommand = new \src\DesignPattern\BehavioralPatterns\Command\Tasks\ClientCommandPattern();
-$addTaskCommand->add();
-echo "<br>";
-echo "/** end command pattern  */";
-/** end command pattern  */
+/**
+ * @return void
+ */
+function demoCommandPattern(): void
+{
+    echo "<br>/** Command Pattern */<br>";
+    $addTaskCommand = new ClientCommandPattern();
+    $addTaskCommand->add();
+}
 
-
+echo "<br>Starting Design Patterns Demonstrations<br>";
+demoFactoryMethodPattern();
+demoAbstractFactoryPattern();
+demoBuilderPattern();
+demoTemplateMethodPattern();
+demoCommandPattern();
+echo "<br>End of Design Patterns Demonstrations<br>";
